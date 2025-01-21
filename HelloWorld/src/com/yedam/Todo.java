72dbfcf77182d8a3e5d97f3c8bf51fa0b39d28ea
youhwan <qjdxjd0643@naver.com>
2025-01-22 00:01:10 +0900

    01-22 2

--- HelloWorld/src/com/yedam/Todo.java
+++ HelloWorld/src/com/yedam/Todo.java
@@ -8,25 +8,30 @@ public class Todo {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int balance = 0;
+		int sum =0;
+//			int num = Integer.parseInt(scn.nextLine()); // 문자타입반환. -> int 변환 Integer.parseInt();
 		while (true) {
 			System.out.println("1.입금 2.출금 3.잔액조회 4.종료");
-			System.out.println("메뉴를 선택하세요>");
-			int num = Integer.parseInt(scn.nextLine()); // 문자타입반환. -> int 변환 Integer.parseInt();
+			System.out.print("메뉴를 선택하세요>");
+			int num = scn.nextInt();
 			
 			if (num == 1) {
 				System.out.println("입금화면입니다.");
-				System.out.println("입금액을 입력.");
-				balance += scn.nextInt();
+				System.out.print("입금액을 입력.");
+				sum += scn.nextInt();
+				balance += sum;
+				
 			} else if (num == 2) {
 				System.out.println("출금화면입니다.");
-				System.out.println("출금액을 입력.");
+				System.out.print("출금액을 입력.");
 				balance -= scn.nextInt();
 			} else if (num == 3) {
-				System.out.println("잔액조회입니다.");
+				System.out.print("잔액조회입니다.");
 				System.out.printf("현재 잔액은 %d입니다.\n", balance);
 			} else if (num == 4){
-				return;
+				break;
 			}
+			break;
 		}
 
 		// nextLine() , nextInt()
