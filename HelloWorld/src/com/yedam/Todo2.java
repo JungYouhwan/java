14bf134f1933e5da88db978bfcc6471462b382bc
youhwan <qjdxjd0643@naver.com>
2025-01-22 19:51:11 +0900

    01-22과제

--- HelloWorld/src/com/yedam/Todo2.java
+++ HelloWorld/src/com/yedam/Todo2.java
@@ -33,9 +33,8 @@ public class Todo2 {
 				}
 				break;
 			case 2:
-				System.out.println("등록화면 입니다.");
-				System.out.println("==================================");
-				System.out.println("이름, 전화번호, 성별을 입력해주세요. ex)홍길동,010-2222-2321,남");
+				System.out.println("등록화면 입니다. 이름, 전화번호, 성별을 입력해주세요.");
+				System.out.println("ex)홍길동,010-2222-2321,남");
 				System.out.print("==================================");
 				for (int i = 0; i <= friend.length; i++) {
 					if (friend[i] == null) {
@@ -48,9 +47,7 @@ public class Todo2 {
 				}
 				break;
 			case 3:
-				System.out.println("조회합니다.");
-				System.out.println("==================================");
-				System.out.println("조회할 성별을 찾으세요");
+				System.out.println("조회 화면입니다. 조회할 성별을 입력해주세요");
 				System.out.print("==================================");
 				String searchEx = scn.nextLine();
 				String ex = null;
@@ -65,25 +62,45 @@ public class Todo2 {
 						System.out.println("==================================");
 					}
 				}
+				System.out.println(searchEx);
+				searchEx = null;
 				break;
 			case 4:
-				System.out.println("삭제화면 입니다.");
-				System.out.println("==================================");
-				System.out.println("이름, 전화번호, 성별을 입력해주세요. ex)홍길동,010-2222-2321,남");
+				System.out.println("삭제화면 입니다. 이름을 입력해주세요");
 				System.out.print("==================================");
 				String searchName = scn.nextLine();
 				for (int i = 0; i <= friend.length; i++) {
 					if (friend[i] == null) {
-//						friend[i].split(",")[0];
-						friend[i] = scn.nextLine();
-						System.out.println(friend[i]);
-						System.out.println("등록 완료.");
+						System.out.println("null값");
+						break;
+					}else if(friend[i].split(",")[0].equals(searchName)) {
+						friend[i] = null;
+						System.out.println("삭제 완료.");
 						System.out.println("==================================");
 						break;
 					}
 				}
+				searchName = null;
 				break;
 			case 5:
+				System.out.println("수정화면 입니다. 이름을 입력해주세요");
+				System.out.print("==================================");
+				String search = scn.nextLine();
+				for (int i = 0; i <= friend.length; i++) {
+					if (friend[i] == null) {
+						break;
+					}else if(friend[i].split(",")[0].equals(search)) {
+						System.out.println("이름,전화번호,성별(남자,여자)입력해주세요.");
+						System.out.print("==================================");
+						String name = scn.nextLine();
+						friend[i] = name;
+						System.out.println("수정 완료.");
+						System.out.println("==================================");
+						name = null;
+						break;
+					}
+				}
+				break;
 			case 9:
 				run = false;
 				break;
