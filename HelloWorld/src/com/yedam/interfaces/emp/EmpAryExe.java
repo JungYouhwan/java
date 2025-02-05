3af984d36734b972311597903cb3e5595eece643
youhwan <qjdxjd0643@naver.com>
2025-02-05 17:50:19 +0900

    02-05

--- HelloWorld/src/com/yedam/interfaces/emp/EmpAryExe.java
+++ HelloWorld/src/com/yedam/interfaces/emp/EmpAryExe.java
@@ -5,8 +5,14 @@ package com.yedam.interfaces.emp;
  */
 public class EmpAryExe implements EmpDAO{
 	
-	Employee[] employees = new Employee[10]; // 저장.
-	
+//	Employee[] employees = new Employee[10]; // 저장.
+	Employee[] employees = {
+		// 초기값
+		new Employee(1001, "홍길동", "432-1234"),
+		new Employee(1012, "조길동", "621-1214"),
+		new Employee(1023, "최길동", "052-5684"),
+		new Employee(1010, "박사장", "333-5678", "2000-01-01", 500)
+	};
 	@Override
 	public boolean registerEmp(Employee emp) {
 		// TODO Auto-generated method stub
@@ -27,7 +33,14 @@ public class EmpAryExe implements EmpDAO{
 
 	@Override
 	public Employee[] search(Employee emp) {
-		// TODO Auto-generated method stub
+		Employee[] result = new Employee[10];
+		int idx = 0;
+		for (int i = 0; i<employees.length; i++) {
+			if(employees[i].getEmpName().indexOf(emp.getEmpName()) > -1) {
+				System.out.println(employees[idx]);
+				idx++;
+			}
+		}
 		return null;
 	}
 
